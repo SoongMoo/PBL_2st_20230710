fee5001a40933169239eae9125895a1cd6f14258
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2023-07-12 16:00:54 +0900

    Add files via upload

--- java/PBL20230712/src/ifexample/SwitchCase1.java
+++ java/PBL20230712/src/ifexample/SwitchCase1.java
@@ -3,7 +3,24 @@ package ifexample;
 public class SwitchCase1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 지역번호에 해당하는 지역을 출력하시오
+		String  num = "02"; 
+		switch(num) {
+		case "02":
+			System.out.println("서울");
+			break;
+		case "031":
+			System.out.println("경기도");
+			break;
+		case "032":
+			System.out.println("인천");
+			break;
+		case "052":
+			System.out.println("울산");
+			 break;
+		case "051":
+			System.out.println("부산");
+		}
 
 	}
 
