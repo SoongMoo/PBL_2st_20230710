6fe4630a9722d2f1c1059fab60aa8fede806d0eb
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2023-07-13 17:46:11 +0900

    Add files via upload

--- java/PBL20230713_1/src/loopexample/ForExample11.java
+++ java/PBL20230713_1/src/loopexample/ForExample11.java
@@ -52,7 +52,7 @@ public class ForExample11 {
 		   ***
 		    *
 		*/
-		int n = 5;  // 패턴의 크기
+		int n = 3;  // 패턴의 크기
 		for (int i = 1; i <= 2 * n - 1; i++) {
 		    int rowCount = i <= n ? i : 2 * n - i;
 		    int spaceCount = n - rowCount;
@@ -67,7 +67,7 @@ public class ForExample11 {
 		}
 		//////////////////////////////////////////
 		/*
-		int n = 5;  // 패턴의 크기
+		int n = 3;  // 패턴의 크기
 		for (int i = 1; i <= n; i++) {
 		    for (int j = 1; j <= n - i; j++) {
 		        System.out.print(" ");
