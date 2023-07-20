b46dabda9192ab62a7ac79b05bc2e6d472d4b5a0
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2023-07-20 16:10:45 +0900

    Add files via upload

--- java/PBL20230720/src/inheritance1/Calculator2.java
+++ java/PBL20230720/src/inheritance1/Calculator2.java
@@ -1,5 +1,7 @@
 package inheritance1;
 
-public class Calculator2 {
-
+public class Calculator2 extends Calculator1{
+	public void sub(int num1, int num2) {
+		result = num1 - num2;
+	}
 }
--- java/PBL20230720/src/inheritance1/Calculator3.java
+++ java/PBL20230720/src/inheritance1/Calculator3.java
@@ -1,5 +1,7 @@
 package inheritance1;
 
-public class Calculator3 {
-
+public class Calculator3 extends Calculator2 {
+	public void mul(int num1, int num2) {
+		result = num1 * num2;
+	}
 }
