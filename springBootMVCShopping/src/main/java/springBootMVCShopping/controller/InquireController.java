54a815d1382814a66aab932a4b08bd420b5bb2d0
hk002 <hk002@DESKTOP-R9I9TP1>
2023-11-10 10:55:33 +0900

    writer

--- springBootMVCShopping/src/main/java/springBootMVCShopping/controller/InquireController.java
+++ springBootMVCShopping/src/main/java/springBootMVCShopping/controller/InquireController.java
@@ -3,6 +3,7 @@ package springBootMVCShopping.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,4 +22,18 @@ public class InquireController {
 		model.addAttribute("newLineChar", "\n");
 		return "thymeleaf/inquire/inquireList";
 	}
+	@GetMapping("inquireWrite")
+	public String inquireWrite(
+			@ModelAttribute(value="goodsNum") String goodsNum) {
+		return "thymeleaf/inquire/inquireWrite";
+	}
 }
+
+
+
+
+
+
+
+
+
